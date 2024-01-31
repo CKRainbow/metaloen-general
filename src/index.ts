e8b593ebcc8f0895369bd240db43a893b57dec67
CKRainbow <nanashichi@proton.me>
2024-01-31 09:45:54 +0800

    fix: 修复对应aid获取

--- src/index.ts
+++ src/index.ts
@@ -17,19 +17,24 @@ export function apply(ctx: Context) {
       const match = source.match(AtRegex);
       const session = argv.session;
 
-      console.log(source);
-      console.log(match);
-
       if (!match || match[1] !== session.bot.userId) return ``;
 
-      const auth = await ctx.database.get("user", 0, ["authority"]);
+      const uid = session.event.user.id;
 
-      if (auth[0].authority >= 2) {
+      const aidPick = await ctx.database.get("binding", uid, ["aid"]);
+
+      if (aidPick.length <= 0) return `账号未自动添加，请重试。`;
+
+      const aid = aidPick[0].aid;
+
+      const authPick = await ctx.database.get("user", aid, ["authority"]);
+
+      if (authPick[0].authority >= 2) {
         await session.send("已经注册过了");
         return;
       }
 
-      const result = await ctx.database.set("user", 0, { authority: 2 });
+      const result = await ctx.database.set("user", aid, { authority: 2 });
 
       if (result.modified === 1)
         await session.send(h("at", { name: session.username }) + "注册成功");
